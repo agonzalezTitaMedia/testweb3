2b5f377e766467b6a59249104e0a54b51785af98
Andres Gonzalez <agonzalez@titamedia.com>
2022-09-12 12:27:42 -0500

    ajustes responsive timeline steps

--- src/componets/Evolution.js
+++ src/componets/Evolution.js
@@ -7,9 +7,9 @@ const Evolution = () => {
             <h3 className="definition-title">Evolución de tu inversión en NFT´s</h3>
             <p className="definition-txt">Un nft funciona como un activo financiero que se transa en el mercado por lo que su precio sube de acuerdo a la demanda</p>
             <div className="evolution-wrapTimeLine">
-                <div className="evolution-colTimeLine">
+            
 
-                    <div className="evolution-stepTimeLine">
+                    <div className="evolution-stepTimeLine evolution-stepTimeLine--uno">
                         <div className="evolution-wrapTxtTimeLine">
                             <h4 className="evolution-titleStep"> <span>1</span> Creacion de los nfts</h4>
                             <p className="evolution-subTitleStep">Optimización de la plataforma de lanzamiento</p>
@@ -30,7 +30,7 @@ const Evolution = () => {
 
 
 
-                    <div className="evolution-stepTimeLine">
+                    <div className="evolution-stepTimeLine evolution-stepTimeLine--tres">
                         <div className="evolution-wrapTxtTimeLine">
                             <h4 className="evolution-titleStep"> <span>3</span> Creacion de los nfts</h4>
                             <p className="evolution-subTitleStep">Optimización de la plataforma de lanzamiento</p>
@@ -48,10 +48,10 @@ const Evolution = () => {
                         </div>
                     </div>
                     
-                </div>
-                <div className="evolution-colTimeLine">
+                
+                
 
-                    <div className="evolution-stepTimeLine evolution-stepTimeLine--right">
+                    <div className="evolution-stepTimeLine evolution-stepTimeLine--dos">
                         <div className="evolution-wrapTxtTimeLine">
                             <h4 className="evolution-titleStep"> <span>2</span> Creacion de los nfts</h4>
                             <p className="evolution-subTitleStep">Optimización de la plataforma de lanzamiento</p>
@@ -70,7 +70,7 @@ const Evolution = () => {
 
 
 
-                    <div className="evolution-stepTimeLine evolution-stepTimeLine--right">
+                    <div className="evolution-stepTimeLine evolution-stepTimeLine--cuatro">
                         <div className="evolution-wrapTxtTimeLine">
                             <h4 className="evolution-titleStep"> <span>4</span> Creacion de los nfts</h4>
                             <p className="evolution-subTitleStep">Optimización de la plataforma de lanzamiento</p>
@@ -87,7 +87,7 @@ const Evolution = () => {
                         </div>
                     </div>
 
-                </div>
+                
 
             </div>
         </div>
