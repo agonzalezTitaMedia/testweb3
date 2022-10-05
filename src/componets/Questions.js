7e9554b89abc5484586f8b2a398785627d69c797
Andres Gonzalez <agonzalez@titamedia.com>
2022-10-05 11:52:34 -0500

    mas texto de preguntas

--- src/componets/Questions.js
+++ src/componets/Questions.js
@@ -17,40 +17,51 @@ const Questions = () => {
                             <div>¿Cómo funcionan los NFT’s?</div>
                             <span className="questions-downQuestion"></span>
                         </div>
-                        <div className={ numberQuestion == 1 ? "questions-botRow show" : "questions-botRow" }>
+                        <div className={ numberQuestion === 1 ? "questions-botRow show" : "questions-botRow" }>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
                         </div>
                     </div>
 
                     <div className="questions-rowQuestions">
                         <div className="questions-topRow"  onClick={()=>{clickQuestion(2)}}>
-                            <div>¿Pregunta 2?</div>
+                            <div>¿Qué es Blockchain?</div>
                             <span className="questions-downQuestion"></span>
                         </div>
-                        <div className={ numberQuestion == 2 ? "questions-botRow show" : "questions-botRow" }>
+                        <div className={ numberQuestion === 2 ? "questions-botRow show" : "questions-botRow" }>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
                         </div>
                     </div>
 
                     <div className="questions-rowQuestions">
                         <div className="questions-topRow"  onClick={()=>{clickQuestion(3)}}>
-                            <div>¿Pregunta 3?</div>
+                            <div>¿Qué es una billetera digital?</div>
                             <span className="questions-downQuestion"></span>
                         </div>
-                        <div className={ numberQuestion == 3 ? "questions-botRow show" : "questions-botRow" }>
+                        <div className={ numberQuestion === 3 ? "questions-botRow show" : "questions-botRow" }>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
                         </div>
                     </div>
 
                     <div className="questions-rowQuestions">
                         <div className="questions-topRow"  onClick={()=>{clickQuestion(4)}}>
-                            <div>¿Pregunta 4?</div>
+                            <div>¿Cómo comprar un NFT?</div>
                             <span className="questions-downQuestion"></span>
                         </div>
-                        <div className={ numberQuestion == 4 ? "questions-botRow show" : "questions-botRow" }>
+                        <div className={ numberQuestion === 4 ? "questions-botRow show" : "questions-botRow" }>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
                         </div>
                     </div>
+
+                    <div className="questions-rowQuestions">
+                        <div className="questions-topRow"  onClick={()=>{clickQuestion(5)}}>
+                            <div>¿De qué me sirve tener un NFT de Mario Hernández?</div>
+                            <span className="questions-downQuestion"></span>
+                        </div>
+                        <div className={ numberQuestion === 5 ? "questions-botRow show" : "questions-botRow" }>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
+                        </div>
+                    </div>
+
                 </div>
             </div>
             
